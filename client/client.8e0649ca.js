59978dc180838845fb680ab17a75159712d73123
Tomasz <tomek.dziopa@gmail.com>
2020-09-15 10:08:10 +0200

    youre my type

---
+++ client/client.8e0649ca.js
@@ -0,0 +1,1 @@
+import"./client.9caea21e.js";
---
+++ client/coda.13c8f312.js
@@ -0,0 +1,1 @@
+import{S as s,i as e,s as t}from"./client.9caea21e.js";export default class extends s{constructor(s){super(),e(this,s,null,null,t,{})}}
